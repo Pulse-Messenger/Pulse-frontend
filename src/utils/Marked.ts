4e4505fa4187aa3d22559bbe2be3070eef15beec
dstuden <studen.david@gmail.com>
2023-06-05 21:15:02 +0200

    markdown changes

--- src/utils/Marked.ts
+++ src/utils/Marked.ts
@@ -40,36 +40,35 @@ const walkTokens = async (token: any) => {
 
     try {
       const tmp = href.split("/");
-      const title = tmp[tmp.length - 1].split(".")[0];
+      const title = tmp[tmp.length - 1];
+
+      // If the link is not a file, return
+      if (!href.match(/^https?:\/\/.*[\\/][^?#]*\.([a-zA-Z0-9]+)\??#?/)[1])
+        return;
 
       const imgs = ["jpeg", "jpg", "png", "gif", "webp"];
-
-      let img = false;
+      let isImg = false;
       imgs.forEach((ext) => {
         if (href.endsWith(ext)) {
           token.type = "image";
           token.title = title;
-
-          img = true;
+          isImg = true;
         }
       });
 
       const vids = ["mp4", "webm"];
-
-      let vid = false;
+      let isVid = false;
       vids.forEach((ext) => {
         if (href.endsWith(ext)) {
           token.type = "html";
           token.text = `<video controls><source src="${href}" type="video/${ext}"></video>`;
           token.title = title;
-
-          vid = true;
+          isVid = true;
         }
       });
 
       const auds = ["mp3", "wav", "flac", "opus"];
-
-      let aud = false;
+      let isAud = false;
       auds.forEach((ext) => {
         if (href.endsWith(ext)) {
           token.type = "html";
@@ -80,27 +79,23 @@ const walkTokens = async (token: any) => {
             .split("\n")
             .join("");
           token.title = title;
-
-          aud = true;
+          isAud = true;
         }
       });
 
-      if (href.split("/").pop().split(".").length > 1 && !img && !vid && !aud) {
-        token.type = "html";
-        token.text = `
+      if (isImg || isVid || isAud) return;
+      token.type = "html";
+      token.text = `
           <div class="file">
-            <a class="name no-txt-overflow" href="${href}" title="${title}">${title}</a>
             <a class="icon" href="${href}" title="${title}">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M64 0C28.7 0 0 28.7 0 64V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V160H256c-17.7 0-32-14.3-32-32V0H64zM256 0V128H384L256 0zM216 232V334.1l31-31c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-72 72c-9.4 9.4-24.6 9.4-33.9 0l-72-72c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l31 31V232c0-13.3 10.7-24 24-24s24 10.7 24 24z"/></svg>
             </a>
+            <a class="name no-txt-overflow" href="${href}" title="${title}">${title}</a>
           </div>
         `
-          .split("\n")
-          .join("");
-        token.title = title;
-      }
-
-      return;
+        .split("\n")
+        .join("");
+      token.title = title;
     } catch (err) {
       return;
     }
